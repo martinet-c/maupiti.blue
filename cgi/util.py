84fc9b34ed27ff784f9a4565300f1b4d450d91a5
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-03-11 16:34:07 +0100

    hey links

--- cgi/util.py
+++ cgi/util.py
@@ -2,7 +2,7 @@ def header():
     print("20 text/gemini", end="\r\n")
 
 def footer_fr():
-    print("=> /index.gmi retour à la page d'accueil")
+    print("=> / retour à la page d'accueil")
     
 def footer_en():
-    print("=> /index.gmi back to home page")
+    print("=> / back to home page")
