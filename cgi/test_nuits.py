848964d515fec8bbcd6ae5df2713b6363be03190
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-03-18 18:59:50 +0100

    testip

--- cgi/test_nuits.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import util
-util.header()
-import requests
-resp = requests.get('http://lesnuitsdunedemoiselle.free.fr/test.php')
-print(resp.text)
---
+++ cgi/testip.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import util
+util.header()
+from os import environ
+print(environ)
