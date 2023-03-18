abc649ea158762d7ff04c07fb0b05773a9057c40
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-03-18 16:48:49 +0100

    aimer3

--- cgi/lesnuitsdunedemoiselle/aimer.py
+++ cgi/lesnuitsdunedemoiselle/aimer.py
@@ -7,7 +7,10 @@ from sys import exit
 if "QUERY_STRING" not in environ:
     print("Mauvais paramètres :(")
     exit()
-params = unquote(environ["QUERY_STRING"]).split('&')
+params = unquote(environ["QUERY_STRING"]).split("&")
+json = "{"
 for sparam in params:
-    print(sparam)
-    
+    param = sparam.split("&")
+    json += '"' + param[0] + '":"' + param[1] + '", '
+json = json[0, len(json)] + "}"
+print(json)
