9abba84901dc9ce195f12a77892efc05265a901c
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-03-18 18:26:30 +0100

    IP Address

--- cgi/lesnuitsdunedemoiselle/aimer.py
+++ cgi/lesnuitsdunedemoiselle/aimer.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 import util
 util.header()
+
+import socket
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+print('IP Address:{}'.format(ip_address))
+
 from urllib.parse import unquote
 from os import environ
 from sys import exit
 if "QUERY_STRING" not in environ:
     print("Mauvais paramètres :(")
     exit()
-import json
 from urllib.parse import parse_qs
-print(json.dumps(parse_qs(environ["QUERY_STRING"])))
-
-#params = unquote(environ["QUERY_STRING"]).split("&")
-#jparams = "{"
-#for sparam in params:
-#    param = sparam.split("=")
-#    jparams += '"' + param[0] + '":"' + param[1] + '", '
-#jparams = jparams[0:len(jparams)-2] + "}"
-#print(jparams)
 import requests
 resp = requests.post("http://lesnuitsdunedemoiselle.free.fr/api/vote.php", data = parse_qs(environ["QUERY_STRING"]))
 print(resp.text)
