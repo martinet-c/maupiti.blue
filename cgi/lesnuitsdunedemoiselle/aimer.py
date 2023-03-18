c1c8fac1497f903047bfc6212f4292e881c153b4
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-03-18 20:50:10 +0100

    nettoyage

--- cgi/lesnuitsdunedemoiselle/aimer.py
+++ cgi/lesnuitsdunedemoiselle/aimer.py
@@ -2,16 +2,13 @@
 import util
 util.header()
 from os import environ
-from urllib.parse import parse_qs
-data = parse_qs(environ["QUERY_STRING"])
-import json
-print(json.dumps(data))
-data["userip"] = environ["REMOTE_ADDR"]
-print(json.dumps(data))
 from sys import exit
 if "QUERY_STRING" not in environ:
     print("Mauvais paramètres :(")
     exit()
+from urllib.parse import parse_qs
+data = parse_qs(environ["QUERY_STRING"])
+data["userip"] = environ["REMOTE_ADDR"]
 import requests
-resp = requests.post("http://lesnuitsdunedemoiselle.free.fr/api/vote.php", data = parse_qs(environ["QUERY_STRING"]))
+resp = requests.post("http://lesnuitsdunedemoiselle.free.fr/api/vote.php", data = data)
 print(resp.text)
