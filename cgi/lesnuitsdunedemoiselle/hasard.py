a3de16a41ef0f912e902037e180ac6291075dd52
MARTINET-C <charlie.martinet@harmonie-mutuelle.fr>
2023-03-20 21:04:06 +0100

    com

--- cgi/lesnuitsdunedemoiselle/hasard.py
+++ cgi/lesnuitsdunedemoiselle/hasard.py
@@ -2,6 +2,6 @@
 page = "hasard"
 import util
 util.header(page, "28 nuits au hasard (rafraichissez la page pour 28 nouvelles propositions)")
-print("Vous aimez une des propositions ci-dessous ? cliquez-la pour l'aimer et l'ajouter à vos favoris !")
+print("Une des propositions ci-dessous vous plaît ? cliquez-la pour l'aimer et l'ajouter à vos favoris !")
 util.print_nights(page, False, True)
 util.footer()
